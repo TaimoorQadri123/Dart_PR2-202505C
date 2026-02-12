10e8734cd5aaf6459205557d856f3d6601c4d2e4
TaimoorQadri123 <dchota374@gmail.com>
2026-02-12 06:13:44 +0500

    just checking

---
+++ main.dart
@@ -0,0 +1,26 @@
+
+
+// First class 
+
+
+// void main(){
+//   print("Taimoor");
+// }
+
+// Second Class 
+// Data Types in Dart 
+ void main(){
+ String name = "Taimoor";
+ print(name);
+ int number = 320;
+ print(number);
+ double salary = 200000;
+ print(salary);
+ num value = 100000.20;
+ print(value);
+ bool isLogin = false;
+ print(isLogin);
+
+
+
+ }
