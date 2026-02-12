d0c18721003a1e1c88e0c378076261d131e763b9
TaimoorQadri123 <dchota374@gmail.com>
2026-02-12 07:06:43 +0500

    donme

--- main.dart
+++ main.dart
@@ -8,19 +8,39 @@
 // }
 
 // Second Class 
-// Data Types in Dart 
+// Variables and Data Types in Dart 
  void main(){
- String name = "Taimoor";
- print(name);
- int number = 320;
- print(number);
- double salary = 200000;
- print(salary);
- num value = 100000.20;
- print(value);
- bool isLogin = false;
- print(isLogin);
+  // Basic DataTypes //
+
+//  String name = "Taimoor";
+//  print(name);
+//  int number = 320;
+//  print(number);
+//  double salary = 200000;
+//  print(salary);
+//  num value = 100000.20;
+//  print(value);
+//  bool isLogin = false;
+//  print(isLogin);
+
+// Collections //
+// isme multiple datatypes store ho sakhty hein 
+
+// isme just normally stores hossakhty hein 
+List Alp = ["A","B","C",1,10.0,true];
+print(Alp);
+
+
+// isme key value hote hein 
+Map Profile = {"name":"ALi","Age":"20"};
+print(Profile);
+
+
+// isme ye hotea hy k duplicate print nhi krta unique rakhta hy  
+Set Names = {"A","A","a","B","B","b","C",1,10.0,true};
+print(Names);
 
 
 
- }
+
+}
