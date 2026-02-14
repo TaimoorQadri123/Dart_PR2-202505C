e7854a0e253646fe48760813ed787494a5ca89cf
TaimoorQadri123 <dchota374@gmail.com>
2026-02-13 23:38:29 -0800

    class 3 with video

--- main.dart
+++ main.dart
@@ -7,9 +7,13 @@
 //   print("Taimoor");
 // }
 
+
+
+
+
 // Second Class 
 // Variables and Data Types in Dart 
- void main(){
+//  void main(){
   // Basic DataTypes //
 
 //  String name = "Taimoor";
@@ -27,20 +31,52 @@
 // isme multiple datatypes store ho sakhty hein 
 
 // isme just normally stores hossakhty hein 
-List Alp = ["A","B","C",1,10.0,true];
-print(Alp);
+// List Alp = ["A","B","C",1,10.0,true];
+// print(Alp);
 
 
 // isme key value hote hein 
-Map Profile = {"name":"ALi","Age":"20"};
-print(Profile);
+// Map Profile = {"name":"ALi","Age":"20"};
+// print(Profile);
 
 
 // isme ye hotea hy k duplicate print nhi krta unique rakhta hy  
-Set Names = {"A","A","a","B","B","b","C",1,10.0,true};
-print(Names);
+// Set Names = {"A","A","a","B","B","b","C",1,10.0,true};
+// print(Names);
 
 
 
 
+// }
+
+
+
+// third class 
+
+  // Var 
+  // dynamic
+  // const
+
+void main(){
+ 
+//  const na data type change krne deta hy aur na hi value 
+ Const String name = "ALi";
+ print(name);
+
+// var value change krta hy  uski data type bhi  krta hy change
+
+dynamic value = 200;
+value =200.330;
+value = "ali";
+
+// var value change krta hy but uski data type fixed krta hy 
+var salary =20000.0;
+salary = 25000;
+salary = "Ali";
+
+
+ List<dynamic> values = ["a","b",200,200.03,true,[]];
+ print(values.runtimeType);
+
+ 
 }
